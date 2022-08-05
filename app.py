be8fae90271c3a1b7ffe7eab280577457d7102b8
Margaret Sun <hi.margaret.sun@gmail.com>
2022-08-05 15:39:21 -0700

    completed step 3

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 """Flask app for adopt app."""
 
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
+
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -30,3 +32,27 @@ def load_root_page():
     all_pets = Pet.query.all()
     return render_template("index.html", pets = all_pets)
 
+@app.route('/add', methods = ['GET', 'POST'])
+def add_pet():
+    """Pet add form; handle adding"""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        new_pet = Pet(name = name, species = species,photo_url = photo_url,
+                      age = age, notes = notes)
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash(f"Added {name}")
+        return redirect("/add")
+
+    else:
+        return render_template("add.html", form = form)
+        # why does it not work if the html is named add_pet.html?
--- forms.py
+++ forms.py
@@ -1,1 +1,28 @@
-"""Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Optional, Email
+
+
+class AddPetForm(FlaskForm):
+    """Forms for adopt app."""
+
+
+    name = StringField("Pet name:",
+        validators= [InputRequired()])
+
+    species = StringField("Species",
+        validators= [InputRequired()])
+
+    photo_url = StringField("Photo URL",
+        validators=[Optional()])
+
+    age = SelectField("Age:",
+        choices= [('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'),
+                  ('senior', 'Senior')],
+        validators= [InputRequired()])
+        
+    notes = TextAreaField("Notes:",
+        validators=[Optional()])
+
+
+
